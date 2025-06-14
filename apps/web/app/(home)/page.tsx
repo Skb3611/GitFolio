0f9932a05f5eb2f97f161e861cdc05b10a817a7f
SKB <shubhambhilare4899223@gmail.com>
2025-06-15 02:33:55 +0530

    Added contact section

--- apps/web/app/(home)/page.tsx
+++ apps/web/app/(home)/page.tsx
@@ -10,7 +10,7 @@ export default function Page() {
     <HeroSection/>
     <GridView/>
     <DevicesSection/>
-    {/* <ContactSection/> */}
+    <ContactSection/>
     <CTASection/>
     </div>
   )
--- apps/web/components/Home/ContactSection.tsx
+++ apps/web/components/Home/ContactSection.tsx
@@ -9,26 +9,26 @@ import React from "react";
 import {MagicCard} from "@workspace/ui/components/magicui/magic-card"
 const ContactSection = () => {
   return (
-    <div id="contact" className="min-w-7xl mx-auto my-20">
-      <h2 className="text-6xl font-semibold w-full mb-2 text-center">
+    <div id="contact" className="w-full lg:max-w-7xl mx-auto my-20 p-4 sm:p-8 overflow-hidden">
+      <h2 className="text-3xl md:text-6xl font-semibold w-full mb-2 text-center">
         We're Just a Message Away
       </h2>
       <AnimatedShinyText className="w-full mx-auto">
-        <p className="text-center max-w-xl mx-auto text-lg">
+        <p className="text-center md:max-w-xl mx-auto text-sm sm:text-lg">
           Whether it's feedback, a feature request, or a quick hello — find us
           on LinkedIn or X.
         </p>
       </AnimatedShinyText>
-      <div className="max-w-3xl mx-auto my-10 flex gap-10">
-      <div className="relative h-full w-1/2 rounded-xl">
+      <div className="w-full lg:max-w-3xl mx-auto my-10 flex flex-col  sm:flex-row gap-10 justify-center items-center">
+      <div className="relative h-full lg:w-1/2 rounded-xl w-full">
         <BorderBeam size={200} initialOffset={0}/>
         <BorderBeam size={200} initialOffset={50}/>
-        <div className="relative flex flex-col space-y-3 items-center border w-full p-8 rounded-xl">
+        <div className="relative flex flex-col space-y-3 items-center border p-4 sm:p-8 rounded-xl">
           <div className="size-20 bg-blue-500 rounded-2xl flex justify-center items-center">
             <Linkedin className="h-12 w-12 text-white" />
           </div>
           <h3 className="text-2xl font-medium">Linkedin</h3>
-          <AnimatedShinyText>
+          <AnimatedShinyText className="text-center">
           Connect with us professionally.
           </AnimatedShinyText>
           <Button variant={"outline"} size={"lg"}>
@@ -36,15 +36,15 @@ const ContactSection = () => {
           </Button>
         </div>
         </div>
-        <div className="relative h-full w-1/2 rounded-xl">
+        <div className="relative h-full lg:w-1/2 rounded-xl w-full">
         <BorderBeam size={200} initialOffset={0}/>
         <BorderBeam size={200} initialOffset={50}/>
-        <div className="relative flex flex-col space-y-3 items-center w-full  p-8 border rounded-xl">
+        <div className="relative flex flex-col space-y-3 items-center  p-4 sm:p-8 border rounded-xl">
           <div className="size-20 bg-white rounded-2xl flex justify-center items-center relative">
             <Image src={"/icons/x.png"} alt={"x"} fill />
           </div>
           <h3 className="text-2xl font-medium">X (Twitter)</h3>
-          <AnimatedShinyText>
+          <AnimatedShinyText className="text-center">
           Follow for real-time updates
           </AnimatedShinyText>
           <Button variant={"outline"} size={"lg"}>
