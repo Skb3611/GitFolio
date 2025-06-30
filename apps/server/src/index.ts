b797aa306c1f1f1b65f55287eb3deec872bb574c
SKB <shubhambhilare4899223@gmail.com>
2025-07-01 01:00:32 +0530

    Changed CROS URL

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -21,7 +21,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://gitfolio-alpha.vercel.app"],
+    origin: ["http://localhost:3000", "https://gitfolio-app.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
