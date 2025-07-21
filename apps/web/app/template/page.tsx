57e5ed7126e666fa01d8d14ee5fff83b92637321
SKB <shubhambhilare4899223@gmail.com>
2025-07-21 19:44:19 +0530

    Templates package init

---
+++ apps/web/app/template/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Template1} from '@workspace/templates'
+
+const page = () => {
+  return (
+    <Template1 />
+  )
+}
+
+export default page
---
+++ packages/templates/src/Templates/Template1/template.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from '@workspace/ui/components/button'
+
+const template = () => {
+  return (
+    <div className='bg-red-300 h-screen w-screen flex items-center justify-center'>
+      <Button>Hello there</Button>
+    </div>
+  )
+}
+
+export default template
