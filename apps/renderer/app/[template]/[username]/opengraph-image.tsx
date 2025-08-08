e6bec46b790b051402e920a5ce0d3561fff41438
SKB <shubhambhilare4899223@gmail.com>
2025-08-08 19:41:10 +0530

    Open graph image fix try

--- apps/renderer/app/[template]/[username]/opengraph-image.tsx
+++ apps/renderer/app/[template]/[username]/opengraph-image.tsx
@@ -1,7 +1,7 @@
 import { SITE_URL, USER_IMAGE_ENDPOINT } from "@/app/config";
 import { ImageResponse } from "next/og";
 
-export const alt = "About Acme";
+export const alt = "Gitfolio Open Graph";
 export const size = {
   width: 1200,
   height: 630,
@@ -14,71 +14,103 @@ export default async function Image({
   params: Promise<{ username: string }>;
 }) {
   const { username } = await params;
-  const res = await fetch(`${USER_IMAGE_ENDPOINT}/${username}`);
-  const result = await res.json();
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          fontSize: 48,
-          background: "white",
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          position: "relative",
-        }}
-      >
-        <img
-          src={`${SITE_URL}/assets/banner-card.png`}
-          alt={alt}
-          style={{ width: "100%", height: "100%" }}
-        />
-        {result.status === true && (
-          <div
-            style={{
-              position: "absolute",
-              top: (result.data.profileImg && result.data.profileImg.length>0)? "8%":"85%",
-              right:(result.data.profileImg && result.data.profileImg.length>0)? "50%" : "5%",
+  try {
+    const res = await fetch(`${USER_IMAGE_ENDPOINT}/${username}`);
+    const result = await res.json();
+    return new ImageResponse(
+      (
+        <div
+          style={{
+            fontSize: 48,
+            background: "white",
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            position: "relative",
+          }}
+        >
+          <img
+            src={`${SITE_URL}/assets/banner-card.png`}
+            alt={alt}
+            style={{ width: "100%", height: "100%" }}
+          />
+          {result.status === true && (
+            <div
+              style={{
+                position: "absolute",
+                top:
+                  result.data.profileImg && result.data.profileImg.length > 0
+                    ? "8%"
+                    : "85%",
+                right:
+                  result.data.profileImg && result.data.profileImg.length > 0
+                    ? "50%"
+                    : "5%",
 
-              transform:(result.data.profileImg && result.data.profileImg.length>0)? "translateX(50%)" : "translateX(0%)",
+                transform:
+                  result.data.profileImg && result.data.profileImg.length > 0
+                    ? "translateX(50%)"
+                    : "translateX(0%)",
 
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              gap: "10px",
-            }}
-          >
-            {result.data.profileImg !== null &&
-              result.data.profileImg.length > 0 && (
-                <img
-                  src={result.data.profileImg}
-                  alt=""
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                gap: "10px",
+              }}
+            >
+              {result.data.profileImg !== null &&
+                result.data.profileImg.length > 0 && (
+                  <img
+                    src={result.data.profileImg}
+                    alt=""
+                    style={{
+                      width: "80px",
+                      height: "80px",
+                      borderRadius: "1000px",
+                    }}
+                  />
+                )}
+              {result.data.name !== null && result.data.name.length > 0 && (
+                <span
                   style={{
-                    width: "80px",
-                    height: "80px",
-                    borderRadius: "1000px",
+                    color: "white",
+                    fontSize: "35px",
+                    fontWeight: "bold",
                   }}
-                />
+                >
+                  {result.data.name}'s Portfolio
+                </span>
               )}
-            {result.data.name !== null && result.data.name.length > 0 && (
-              <span
-                style={{
-                  color: "white",
-                  fontSize: "35px",
-                  fontWeight: "bold",
-                }}
-              >
-                {result.data.name}'s Portfolio
-              </span>
-            )}
-          </div>
-        )}
-      </div>
-    ),
-    {
-      ...size,
-    }
-  );
+            </div>
+          )}
+        </div>
+      ),
+      {
+        ...size,
+      }
+    );
+  } catch (e) {
+    console.log("cannot generate image");
+    return new ImageResponse(
+      (
+        <div
+          style={{
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <img
+            src={`${SITE_URL}/assets/banner-card.png`}
+            alt={alt}
+            style={{ width: "100%", height: "100%", objectFit: "contain" }}
+          />
+        </div>
+      )
+    );
+  }
 }
