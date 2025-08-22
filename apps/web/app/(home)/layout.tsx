5562f30c83a7073795f597eccbb4ce40120eb1c1
SKB <shubhambhilare4899223@gmail.com>
2025-08-22 22:58:55 +0530

    Maintainance

--- apps/web/app/(home)/layout.tsx
+++ apps/web/app/(home)/layout.tsx
@@ -1,12 +1,13 @@
 import Footer from "@/components/Footer/Footer";
 import Navbar from "@/components/Navbar";
-
+import Maintenance from "@workspace/ui/components/ui/Maintenance";
 export default function HomeLayout({children}:{children:React.ReactNode}){
 return(
     <>
-    <Navbar/>
-    {children}
-    <Footer/>
+    <Maintenance/>
+    {/* <Navbar/> */}
+    {/* {children} */}
+    {/* <Footer/> */}
     </>
 )
 }
