9ead613484f944c065a09cd412b53b64f2c8e864
SKB <shubhambhilare4899223@gmail.com>
2025-09-09 19:58:13 +0530

    Image changes in Devices Section

--- apps/web/components/Home/DevicesSection.tsx
+++ apps/web/components/Home/DevicesSection.tsx
@@ -4,7 +4,7 @@ import Iphone15Pro from "@workspace/ui/components/magicui/iphone-15-pro";
 import { Safari } from "@workspace/ui/components/magicui/safari";
 import { AnimatedShinyText } from "@workspace/ui/components/magicui/animated-shiny-text";
 import { Particles } from "@workspace/ui/components/magicui/particles";
-import Android from "@workspace/ui/components/magicui/android"
+import Android from "@workspace/ui/components/magicui/android";
 import {
   Carousel,
   CarouselContent,
@@ -13,10 +13,16 @@ import {
 } from "@workspace/ui/components/carousel";
 import SectionLabel from "@workspace/ui/components/SectionLabel";
 
-import {motion} from "motion/react"
+import { motion } from "motion/react";
 import { SplitTextAnimation } from "../SplitTextAnimation";
-
+import { useIsMobile } from "@/hooks/use-mobile";
 const DevicesSection = () => {
+  const isMobile = useIsMobile();
+  const mobileUrl = isMobile
+    ? "https://pub-7e33da773f24477fad91084ffacf40cb.r2.dev/templates/DevPro/preview/mobile-light.png"
+    : "https://pub-7e33da773f24477fad91084ffacf40cb.r2.dev/templates/Black%20&%20White/preview/mobile-light.png";
+  const desktopUrl =
+    "https://pub-7e33da773f24477fad91084ffacf40cb.r2.dev/templates/DevPro/preview/desktop-light.png";
   return (
     <div className="h-full w-full relative">
       <Particles
@@ -29,76 +35,72 @@ const DevicesSection = () => {
 
       <div className="relative h-full my-10 mx-auto max-w-7xl flex gap-4 justify-center items-center flex-col overflow-hidden">
         <SectionLabel title={"✨ Fully Responsive UI"} />
-          <SplitTextAnimation
+        <SplitTextAnimation
           whileInView
           delay={0.2}
           staggerDelay={0.08}
           className="w-[90%] mx-auto sm:w-full text-3xl mb-0 text-center font-semibold md:text-6xl"
-          >
+        >
           Seamless on Every Device
-          </SplitTextAnimation>
-        
+        </SplitTextAnimation>
+
         <AnimatedShinyText className="">
           <SplitTextAnimation
-          className="w-[80%] mx-auto md:w-full text-center text-sm md:text-xl   "
-          whileInView
-          delay={0.2}
+            className="w-[80%] mx-auto md:w-full text-center text-sm md:text-xl   "
+            whileInView
+            delay={0.2}
           >
-          From compact mobiles to widescreen monitors, your portfolio always 
-          looks its best.
+            From compact mobiles to widescreen monitors, your portfolio always
+            looks its best.
           </SplitTextAnimation>
         </AnimatedShinyText>
 
         <div className="relative max-w-4xl xl:max-w-6xl mt-10 mx-auto hidden lg:grid grid-cols-3 gap-5 place-items-center">
           <motion.div
-          className="col-span-1 flex justify-center"
-          initial={{
-            x:-50,
-            opacity:0,
-            scale:0.9,
-            filter:"blur(5px)"
-          }}
-          whileInView={{
-            x:0,
-            opacity:1,
-            scale:1,
-            filter:"blur(0px)"
-          }}
-          viewport={{once:true}}
-          transition={{
-            duration:0.4,
-            delay:0.3
-          }}
-
+            className="col-span-1 flex justify-center"
+            initial={{
+              x: -50,
+              opacity: 0,
+              scale: 0.9,
+              filter: "blur(5px)",
+            }}
+            whileInView={{
+              x: 0,
+              opacity: 1,
+              scale: 1,
+              filter: "blur(0px)",
+            }}
+            viewport={{ once: true }}
+            transition={{
+              duration: 0.4,
+              delay: 0.3,
+            }}
           >
-            <Iphone15Pro
-              src="/assets/phone-white.jpg"
-              className=" w-2/3 h-full "
-            />
+            <Iphone15Pro src={mobileUrl} className=" w-2/3 h-full " />
           </motion.div>
           <motion.div
-          className="col-span-2 relative place-content-center"
-           initial={{
-            x:50,
-            opacity:0,
-            scale:0.9,
-            filter:"blur(5px)"
-          }}
-          whileInView={{
-            x:0,
-            opacity:1,
-            scale:1,
-            filter:"blur(0px)"
-          }}
-          viewport={{once:true}}
-          transition={{
-            duration:0.4,
-            delay:0.3
-          }}
+            className="col-span-2 relative place-content-center"
+            initial={{
+              x: 50,
+              opacity: 0,
+              scale: 0.9,
+              filter: "blur(5px)",
+            }}
+            whileInView={{
+              x: 0,
+              opacity: 1,
+              scale: 1,
+              filter: "blur(0px)",
+            }}
+            viewport={{ once: true }}
+            transition={{
+              duration: 0.4,
+              delay: 0.3,
+            }}
           >
             <Safari
-            height={650}
-              imageSrc="/assets/desktop-white.png"
+              height={650}
+              imageSrc={desktopUrl}
               className="w-full h-full rounded-b-lg"
               mode="simple"
               url="gitfolio.example"
@@ -118,12 +120,12 @@ const DevicesSection = () => {
         >
           <CarouselContent className="w-full">
             <CarouselItem className="flex justify-center items-center ">
-              <Android src="/assets/phone-white.jpg" className="w-[80%] ml-15  " />
+              <Android src={mobileUrl} className="w-[80%] ml-15 z-10 " />
             </CarouselItem>
             <CarouselItem className=" flex justify-center items-center ml-2">
               <Safari
                 // height={700}
-                imageSrc="/assets/desktop-white.png"
+                imageSrc={desktopUrl}
                 className="w-[90%] h-[90%]"
                 mode="simple"
                 url="gitfolio.example"
