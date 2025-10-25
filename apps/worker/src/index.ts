16a906b33639a54d05d05d44b0d2be63369715c7
SKB <shubhambhilare4899223@gmail.com>
2025-10-25 20:46:42 +0530

    Worker converted to dummy express app

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -2,8 +2,17 @@
 import dotenv from "dotenv";
 import { createEmailWorker } from "@workspace/email/queue";
 import { sendEmail } from "@workspace/email";
-
+import express from "express";
 dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Worker is running");
+});
+app.listen(3000, () => {
+  console.log("Worker is running on port 3000");
+});
 
 createEmailWorker(async (job: any) => {
   const { email, name, type } = job.data;
