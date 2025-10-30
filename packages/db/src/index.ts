f9e6563366f454ec3d6fc4a77318a69bd7018e3a
SKB <shubhambhilare4899223@gmail.com>
2025-10-30 12:55:32 +0530

    Added Acelerate to DB

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,11 +1,11 @@
 import { PrismaClient } from "./generated/prisma/index.js";
-
+import {withAccelerate} from "@prisma/extension-accelerate"
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
     log: ['error', 'warn'],
-  });
+  }).$extends(withAccelerate());
 if (process.env.NODE_ENV !== 'prod') globalForPrisma.prisma = prisma;
 
 export default prisma;
