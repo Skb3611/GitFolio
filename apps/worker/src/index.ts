37483acb5b133556fd5a3692f11441253d54d825
SKB <shubhambhilare4899223@gmail.com>
2025-11-01 20:42:57 +0530

    Updated the worker dummy endpoint

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import "./worker";
+import { emailQueue } from "@workspace/email/queue";
 
 const app = express();
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.send("Worker is running");
+app.get("/", async(req, res) => {
+  const count = await emailQueue.getJobCounts()
+  res.send(`Worker is running, ${count} jobs in queue`);
 });
 app.listen(3000, () => {
   console.log("Worker is running on port 3000");
