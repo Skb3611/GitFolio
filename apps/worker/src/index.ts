4f759c811491d92a2026a5c9b3898a93d9334e72
SKB <shubhambhilare4899223@gmail.com>
2025-11-02 02:13:03 +0530

    Dummy endoint worker

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -4,9 +4,9 @@ import { emailQueue } from "@workspace/email/queue";
 
 const app = express();
 app.use(express.json());
-app.get("/", async(req, res) => {
-  const count = await emailQueue.getJobCounts()
-  res.send(`Worker is running, ${count} jobs in queue`);
+app.get("/", async (req, res) => {
+  const count = await emailQueue.getJobCounts();
+  res.send(`Worker is running, ${JSON.stringify(count)} jobs in queue`);
 });
 app.listen(3000, () => {
   console.log("Worker is running on port 3000");
