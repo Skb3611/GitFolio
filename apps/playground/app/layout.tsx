165e3a25fc26c3ad3649d84ea41a8ddb2ed123e9
SKB <shubhambhilare4899223@gmail.com>
2025-11-08 14:39:33 +0530

    Added vercel analytics to playground

--- apps/playground/app/layout.tsx
+++ apps/playground/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Manrope } from "next/font/google";
 import "@workspace/ui/globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const manRope = Manrope({
   display: "swap",
@@ -35,6 +36,7 @@ export default function RootLayout({
         className={`  ${manRope.className} antialiased dark overflow-x-hidden max-w-screen h-full`}
       >
         {children}
+        <Analytics/>
       </body>
     </html>
   );
