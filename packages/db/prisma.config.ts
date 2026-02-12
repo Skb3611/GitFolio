9fe6d911c21dc4825241d0b63e034603f1bc4676
SKB <shubhambhilare4899223@gmail.com>
2026-02-12 14:54:04 +0530

    prisma config changed

--- packages/db/prisma.config.ts
+++ packages/db/prisma.config.ts
@@ -1,12 +1,9 @@
 import 'dotenv/config'
 import { defineConfig, env } from 'prisma/config'
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is missing at build time");
-}
 export default defineConfig({
   datasource: {
-    url: env('DATABASE_URL'),
+    url: process.env.DATABASE_URL!,
     // shadowDatabaseUrl: env('SHADOW_DATABASE_URL')
   },
 })
